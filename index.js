90934a04977a7cde7274f8b5eb5659a6e0935df1
David Burns <david.burns@opengear.com>
2019-02-22 16:36:21 +1000

    Removed input

--- index.js
+++ index.js
@@ -3,16 +3,10 @@ import('./pkg/lorenz_attractor')
         const canvas = document.getElementById('drawing');
         const ctx = canvas.getContext('2d');
 
-        const xInput = document.getElementById('startx');
-        const yInput = document.getElementById('starty');
-        const zInput = document.getElementById('startz');
         const renderBtn = document.getElementById('render');
 
         renderBtn.addEventListener('click', () => {
-            const x = parseFloat(xInput.value) || 0;
-            const y = parseFloat(yInput.value) || 0;
-            const z = parseFloat(zInput.value) || 0;
-            wasm.draw(ctx, 600, 600, x, y, z);
+            wasm.draw(ctx, 600, 600, 0, 0, 0);
         });
 
         wasm.draw(ctx, 600, 600, 0, 0, 0);
